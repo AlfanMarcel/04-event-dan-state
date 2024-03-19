b1f2a436261ff1582f13cf9552b82f71b022824d
AlfanMarcel <alfan.marcel2808@gmail.com>
2024-03-19 14:12:23 +0700

    Praktikum 5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Tombol_1, {Tombol_2, Tombol_3} from '@/components/button';
 import Gallery from "@/components/gallery";
-
+import Form from "@/components/form";
 
 export default function Home() {
   return (
@@ -19,6 +19,8 @@ export default function Home() {
       </div>
       <br></br>
       <Gallery />
+      <br></br>
+      <Form />
     </>
   );
 }
